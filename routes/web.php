7dd64f43773bef1c4d5afa7096e370d4466ae331
itsmhyne <mhamdanydev@gmail.com>
2023-08-29 08:32:02 +0700

    membuat todolistcontroller

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\TodoListController;
 use App\Http\Middleware\OnlyGuestMiddleware;
 use App\Http\Middleware\OnlyMemberMiddleware;
 
@@ -25,3 +26,12 @@ Route::controller(UserController::class)->group(function () {
     Route::post('/login', 'doLogin')->middleware([OnlyGuestMiddleware::class]);
     Route::post('/logout', 'doLogout')->middleware([OnlyMemberMiddleware::class]);
 });
+
+Route::controller(TodoListController::class)
+    ->middleware([OnlyMemberMiddleware::class])
+    ->group(function ()
+    {
+        Route::get('/todolist', 'todoList');
+        Route::post('/todolist', 'addTodo');
+        Route::post('/todolist/{id}/delete', 'removeTodo');
+    });
